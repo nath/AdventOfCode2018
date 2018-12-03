3f53807ecd957f9db189eea2ef6d3b8aa8e1d7d7
Nath Tumlin <ntumlin@gmail.com>
2018-12-03 13:11:08 -0600

    Remove unnecessary conversion to list

--- day2/day2.py
+++ day2/day2.py
@@ -20,7 +20,7 @@ print("Part 1:", two * three)
 import itertools
 
 for line1, line2 in itertools.combinations(lines, 2):
-    differences = set(list(enumerate(line1))) - set(list(enumerate(line2)))
+    differences = set(enumerate(line1)) - set(enumerate(line2))
     if len(differences) == 1:
         i = differences.pop()[0]
         print("Part 2:", line1[:i] + line1[i+1:])
